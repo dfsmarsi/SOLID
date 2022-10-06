f2a47cc52bfd1106b3d3c2e97150ce6ead49d900
dfsmarsi <douglas@sistemasbr.net>
2022-10-06 10:18:58 -0300

    melhorando principio L

--- SOLID/L_SubstituicaoDeLiskov/PagamentoDeSalariosService.cs
+++ SOLID/L_SubstituicaoDeLiskov/PagamentoDeSalariosService.cs
@@ -1,12 +1,22 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 
 namespace SOLID.L_SubstituicaoDeLiskov
 {
     public class PagamentoDeSalariosService
     {
-        public void RealizarPagamentosDeSalario(List<Funcionario> funcionario)
+        public void RealizarPagamentosDeSalario(List<Funcionario> listaDeFuncionarios)
         {
+            foreach (var item in listaDeFuncionarios)
+            {
+                double valor = item.CalcularSalario();
+                RealizarTransacaoBancaria(valor);
+            }
+        }
 
+        private void RealizarTransacaoBancaria(double valor)
+        {
+            throw new NotImplementedException();
         }
     }
 }
